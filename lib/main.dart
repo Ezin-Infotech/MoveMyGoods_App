b53364c39125f52f8467d7714095e4b0a8bee280
ashimshaheer <ashimsha222@gmail.com>
2024-03-11 23:46:57 +0530

    any one pushing chekinh

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:mmg/app/home/view%20model/home_provider.dart';
 import 'package:mmg/app/home/view/global_screen.dart';
 import 'package:mmg/app/utils/app%20style/responsive.dart';
 import 'package:mmg/app/utils/providers.dart';
